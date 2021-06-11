e537da12c5f259495d9fa6286b07b837c607ce36
Thomas Stenner <thomasstenner@thomass-mbp.attlocal.net>
2021-06-10 20:00:05 -0500

    new

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,3 +18,26 @@ const svg = d3
   .attr("width", svgWidth)
   .attr("height", svgHeight + 40); 
   // line 19 is extra padding for the third label
+
+  // Append SVG group
+const chartGroup = svg.append("g")
+.attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Parameters 
+let chosenXAxis = "poverty";
+let chosenYAxis = "healthcare";
+
+(async function(){
+      
+    // Data import 
+    const stateData = await d3.csv("assets/data/data.csv");
+
+    // Parse Data/Cast as numbers
+    stateData.forEach(function(data) {
+      data.poverty    = +data.poverty;
+      data.healthcare = +data.healthcare;
+      data.age        = +data.age;
+      data.smokes     = +data.smokes;
+      data.obesity    = +data.obesity;
+      data.income     = +data.income;
+    });
