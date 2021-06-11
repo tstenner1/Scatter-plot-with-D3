e9595eb83893c191ad78237dbc730fd80ab87d0c
Thomas Stenner <thomasstenner@thomass-mbp.attlocal.net>
2021-06-10 20:59:44 -0500

    new

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -63,3 +63,54 @@ function renderXCircles(circlesGroup, newXScale, chosenXaxis) {
     return circlesGroup;
   }
   
+  // functions used for updating circles text with a transition on new circles for X and Y coordinates
+  function renderXText(circlesGroup, newXScale, chosenXaxis) {
+
+    circlesGroup.transition()
+      .duration(1000)
+      .attr("dx", d => newXScale(d[chosenXAxis]));
+  
+    return circlesGroup;
+  }
+  
+  function renderYText(circlesGroup, newYScale, chosenYaxis) {
+  
+    circlesGroup.transition()
+      .duration(1000)
+      .attr("dy", d => newYScale(d[chosenYAxis])+5);
+  
+    return circlesGroup;
+  }
+
+  // format number to USD currency
+let formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+  
+  // function used for updating circles group with new tooltip
+  function updateToolTip(circlesGroup, chosenXAxis, chosenYAxis) {
+  
+    let xpercentsign = "";
+    let xlabel = "";
+    if (chosenXAxis === "poverty") {
+      xlabel = "Poverty";
+      xpercentsign = "%";
+    } else if (chosenXAxis === "age"){
+      xlabel = "Age";
+    } else {
+      xlabel = "Income";
+    }
+  
+    let ypercentsign = "";
+    let ylabel = "";
+    if (chosenYAxis === "healthcare") {
+      ylabel = "Healthcare";
+      ypercentsign = "%";
+    } else if (chosenYAxis === "smokes"){
+      ylabel = "Smokes";
+      ypercentsign = "%";
+    } else {
+      ylabel = "Obesity";
+      ypercentsign = "%";
+    }
